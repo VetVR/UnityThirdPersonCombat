62ac1221cc3f75dabd06e7e6e92dc904280810f5
Ron H <ronhat@gmail.com>
2022-06-17 07:23:26 -0700

    Before Enemy Idle State.  Enemy Model added.

---
+++ Assets/Scripts/StateMachines/Enemy/EnemyStateMachine.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStateMachine : StateMachine
+{
+    public void Enter()
+    {
+        
+    }
+
+    public void Tick(float deltaTime)
+    {
+        
+    }
+
+    public void Exit()
+    {
+        
+    }
+}
